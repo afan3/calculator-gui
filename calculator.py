f6344d3993ed300541b069c379f3903f78e2b1da
Affan Younis <123302759+afan3@users.noreply.github.com>
2023-02-24 11:30:11 +0530

    add display and buttons frame

--- calculator.py
+++ calculator.py
@@ -1,11 +1,26 @@
 import tkinter as tk
 
+LIGHT_GRAY = '#F5F5F5'
+
 class Calculator:
     def __init__(self) -> None:
         self.window = tk.Tk()
         self.window.geometry('375x667')
         self.window.resizable(0, 0)
         self.window.title('Calculator')
+        
+        self.display_frame = self.create_display_frame()
+        self.buttons_frame = self.create_buttons_frame()
+    
+    def create_display_frame(self):
+        frame = tk.FRame(self.window, height=221, bg=LIGHT_GRAY)
+        frame.pack(expand=True, fill='both')
+        return frame
+    
+    def create_buttons_frame(self):
+        frame = tk.Frame(self.window)
+        frame.pack(expandf=True, fill='both')
+        return frame
     
     def run(self):
         self.window.mainloop()
