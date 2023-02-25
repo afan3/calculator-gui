ad3d87e9bc5045b137eca8273b291f75b638d45b
Affan Younis <123302759+afan3@users.noreply.github.com>
2023-02-25 10:54:43 +0530

    add digits button

--- calculator.py
+++ calculator.py
@@ -2,8 +2,10 @@ import tkinter as tk
 
 LIGHT_GRAY = '#F5F5F5'
 LABEL_COLOR = '#25265E'
+WHITE = '#FFFFFF'
 LARGE_FONT_STYLE = ('Arial', 40, 'bold')
 SMALL_FONT_STYLE = ('Arial', 16)
+DIGITS_FONT_STYLE = ('Arial', 24, 'bold')
 
 
 class Calculator:
@@ -19,7 +21,14 @@ class Calculator:
         self.display_frame = self.create_display_frame()
         self.total_expr_label, self.current_expr_label = self.create_display_labels()
 
+        self.digits = {
+            7: (1, 1), 8: (1, 2), 9: (1, 3),
+            4: (2, 1), 5: (2, 2), 6: (2, 3),
+            1: (3, 1), 2: (3, 2), 3: (3, 3),
+            0: (4, 2), '.': (4, 1)
+        }
         self.buttons_frame = self.create_buttons_frame()
+        self.create_digit_buttons()
     
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
@@ -55,6 +64,11 @@ class Calculator:
         current_expr_label.pack(expand=True, fill='both')
 
         return total_expr_label, current_expr_label
+    
+    def create_digit_buttons(self):
+        for digit, grid_value in self.digits.items():
+            button = tk.Button(self.buttons_frame, text=str(digit), bg=WHITE, fg=LABEL_COLOR, font=DIGITS_FONT_STYLE, borderwidth=0)
+            button.grid(row=grid_value[0], column=grid_value[1], sticky=tk.NSEW)
     
     def run(self):
         self.window.mainloop()
